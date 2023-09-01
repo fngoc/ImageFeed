bb9f666cc3127cc826f6da37485527ebd34add9d
VitMarKha <vitmarkha@icloud.com>
2023-09-01 10:18:54 +0300

    add table logic

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor.swift
+//  ImageFeed
+//
+//  Created by Виталий Хайдаров on 30.08.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var myBackground: UIColor { UIColor(named: "Background") ?? UIColor.black }
+    static var myBlack: UIColor { UIColor(named: "Black") ?? UIColor.black }
+    static var myBlue: UIColor { UIColor(named: "Blue") ?? UIColor.blue }
+    static var myGray: UIColor { UIColor(named: "Gray") ?? UIColor.gray }
+    static var myRed: UIColor { UIColor(named: "Red") ?? UIColor.red }
+    static var myWhite: UIColor { UIColor(named: "White") ?? UIColor.white }
+    static var myWhiteAlpha: UIColor { UIColor(named: "White Alpha") ?? UIColor.white }
+}
