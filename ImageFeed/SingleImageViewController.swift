6450e30e39e739091eaf218391d6fb3655f634f5
VitMarKha <vitmarkha@icloud.com>
2023-09-14 20:08:52 +0300

    show image

--- ImageFeed/SingleImageViewController.swift
+++ ImageFeed/SingleImageViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     
     @IBOutlet weak var imageView: UIImageView!
+    var image: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.image = image
     }
 }
