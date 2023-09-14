7b75feeb6d45c92086f9168992fbeb61428783e1
VitMarKha <vitmarkha@icloud.com>
2023-09-14 15:38:03 +0300

    add SingleImageViewController and SingleImage storyboard

---
+++ ImageFeed/SingleImageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SingleImageViewController.swift
+//  ImageFeed
+//
+//  Created by Виталий Хайдаров on 14.09.2023.
+//
+
+import UIKit
+
+final class SingleImageViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
