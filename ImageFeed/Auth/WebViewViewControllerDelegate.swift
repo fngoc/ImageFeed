85b543d94baaf921235bde5535b33622930d8aa6
VitMarKha <vitmarkha@icloud.com>
2023-11-12 19:50:04 +0300

    continues commit

--- ImageFeed/Auth/WebViewViewControllerDelegate.swift
+++ ImageFeed/Auth/WebViewViewControllerDelegate.swift
@@ -5,4 +5,10 @@
 //  Created by Виталий Хайдаров on 12.11.2023.
 //
 
-import Foundation
+import UIKit
+
+protocol WebViewViewControllerDelegate: AnyObject {
+    
+    func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String)
+    func webViewViewControllerDidCancel(_ vc: WebViewViewController)
+}
